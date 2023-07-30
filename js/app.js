5fc52e08a30f733dd4cc61016c8eb5e22721c509
AlexanderHuisa <alexanderhuisa@gmail.com>
2023-07-29 23:33:41 -0500

    add logica to hidden an show div in features section, change a for button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,23 @@
-const simpleBookmarking = document.querySelector('.simple__book');
-const speedySearching = document.querySelector('.speedy__search');
-const easySharing = document.querySelector('.easy__sharing');
+const BtnSimpleBookmarking = document.querySelector('.simple__book');
+const BtnSpeedySearching = document.querySelector('.speedy__search');
+const BtnEasySharing = document.querySelector('.easy__sharing');
 
-simpleBookmarking.textContent
+const divSimpleB = document.querySelector('.feature__content--simple-bookmarking');
+const divSpeedyS = document.querySelector('.feature__content--speedy-searching');
+const divEasyS = document.querySelector('.feature__content--easy-sharing');
+
+BtnSimpleBookmarking.addEventListener('click',()=>{
+    divSimpleB.style.display = 'block';
+    divSpeedyS.style.display = 'none';
+    divEasyS.style.display = 'none';
+})
+BtnSpeedySearching.addEventListener('click',()=>{
+    divSimpleB.style.display = 'none';
+    divSpeedyS.style.display = 'block';
+    divEasyS.style.display = 'none';
+})
+BtnEasySharing.addEventListener('click',()=>{
+    divSimpleB.style.display = 'none';
+    divSpeedyS.style.display = 'none';
+    divEasyS.style.display = 'block';
+})
