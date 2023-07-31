58f57c6eb595e76010d6f688ce0808de01229882
AlexanderHuisa <alexanderhuisa@gmail.com>
2023-07-30 22:19:51 -0500

    add dinamic to sub line red in section features

--- js/app.js
+++ js/app.js
@@ -10,14 +10,23 @@ BtnSimpleBookmarking.addEventListener('click',()=>{
     divSimpleB.style.display = 'inline-flex';
     divSpeedyS.style.display = 'none';
     divEasyS.style.display = 'none';
+    BtnSimpleBookmarking.classList.add('main__features__nav--sub-line');
+    BtnSpeedySearching.classList.remove('main__features__nav--sub-line');
+    BtnEasySharing.classList.remove('main__features__nav--sub-line');
 })
 BtnSpeedySearching.addEventListener('click',()=>{
     divSimpleB.style.display = 'none';
     divSpeedyS.style.display = 'inline-flex';
     divEasyS.style.display = 'none';
+    BtnSimpleBookmarking.classList.remove('main__features__nav--sub-line');
+    BtnSpeedySearching.classList.add('main__features__nav--sub-line');
+    BtnEasySharing.classList.remove('main__features__nav--sub-line');
 })
 BtnEasySharing.addEventListener('click',()=>{
     divSimpleB.style.display = 'none';
     divSpeedyS.style.display = 'none';
     divEasyS.style.display = 'inline-flex';
+    BtnSimpleBookmarking.classList.remove('main__features__nav--sub-line');
+    BtnSpeedySearching.classList.remove('main__features__nav--sub-line');
+    BtnEasySharing.classList.add('main__features__nav--sub-line');
 })
