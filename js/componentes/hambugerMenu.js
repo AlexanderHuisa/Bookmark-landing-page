12b229bcaead95623b7cf94916a0d80d94db321d
AlexanderHuisa <alexanderhuisa@gmail.com>
2023-08-09 06:21:47 -0500

    add blackground-color to header when clicked to btnHamburger

--- js/componentes/hambugerMenu.js
+++ js/componentes/hambugerMenu.js
@@ -1,7 +1,16 @@
 const btnHamburger = document.querySelector(".hamburger");
 const divMenu = document.querySelector(".header__hero--nav");
+const divHeader = document.querySelector(".header__hero");
+let active = false;
 
 btnHamburger.addEventListener("click", ()=>{
     divMenu.classList.toggle("active");
     btnHamburger.classList.toggle("actived");
+    if (active == false){
+        divHeader.style.backgroundColor = "black"
+        active = true;
+    } else{
+        divHeader.style.backgroundColor = "white";
+        active = false;
+    }
 })
