58ff1dcc5d7cf190fb346367f41a836aac825b7c
AlexanderHuisa <alexanderhuisa@gmail.com>
2023-09-20 23:53:05 -0500

    add file validation

---
+++ js/componentes/validation.js
@@ -0,0 +1,5 @@
+const btnHamburger = document.querySelector(".hamburger");
+const line = document.querySelectorAll(".line");
+const logo = document.querySelector(".logo-header");
+const divMenu = document.querySelector(".header__hero--nav");
+const divHeader = document.querySelector(".header__hero");
